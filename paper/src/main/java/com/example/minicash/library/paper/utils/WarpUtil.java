fb64c8021fa4a24c5df26a90e6ee50a19c44207c
MiniCash <tminecraft2143@gmail.com>
2026-08-15 20:05:28 +0900

    Add files via upload

---
+++ paper/src/main/java/com/example/minicash/library/paper/utils/WarpUtil.java
@@ -0,0 +1,19 @@
+package com.example.minicash.library.paper.utils;
+
+import com.example.minicash.library.paper.MiniCashPaperLibrary;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import org.bukkit.entity.Player;
+
+public class WarpUtil {
+    public static void sendWarpRequest(Player player, String targetServer, String warpPointName, MiniCashPaperLibrary plugin) {
+
+        ByteArrayDataOutput out = ByteStreams.newDataOutput();
+
+        out.writeUTF("RequestWarp");
+        out.writeUTF(targetServer);
+        out.writeUTF(warpPointName);
+
+        player.sendPluginMessage(plugin, MiniCashPaperLibrary.WARP_CHANNEL, out.toByteArray());
+    }
+}
