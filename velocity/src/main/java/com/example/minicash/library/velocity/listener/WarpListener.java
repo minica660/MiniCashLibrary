f5ed43abdb37ab1470a2c8231bf4637bcdf5a216
MiniCash <tminecraft2143@gmail.com>
2026-08-17 18:55:57 +0900

    Add files via upload

--- velocity/src/main/java/com/example/minicash/library/velocity/listener/WarpListener.java
+++ velocity/src/main/java/com/example/minicash/library/velocity/listener/WarpListener.java
@@ -1,6 +1,5 @@
 package com.example.minicash.library.velocity.listener;
 
-import com.example.minicash.library.common.models.WarpResult;
 import com.example.minicash.library.velocity.MiniCashVelocityLibrary;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteArrayDataOutput;
@@ -9,25 +8,32 @@ import com.google.gson.Gson;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.PluginMessageEvent;
 import com.velocitypowered.api.event.player.KickedFromServerEvent;
+import com.velocitypowered.api.event.player.ServerConnectedEvent;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class WarpListener {
 
+    private final MiniCashVelocityLibrary plugin;
     private final ProxyServer proxy;
     private final Gson gson = new Gson();
 
     private final Map<UUID, String> pendingWarpPoints = new ConcurrentHashMap<>();
 
-    public WarpListener(ProxyServer proxy) {
+    public WarpListener(ProxyServer proxy ,  MiniCashVelocityLibrary plugin) {
         this.proxy = proxy;
+        this.plugin = plugin;
     }
 
 
@@ -36,8 +42,7 @@ public class WarpListener {
      */
     @Subscribe
     public void onPluginMessage(PluginMessageEvent event) {
-
-        if (!event.getIdentifier().equals(MiniCashVelocityLibrary.WARP_CHANNEL)){
+        if (!event.getIdentifier().equals(MiniCashVelocityLibrary.WARP_CHANNEL)) {
             return;
         }
 
@@ -45,110 +50,110 @@ public class WarpListener {
         String subChannel = in.readUTF();
 
         if ("RequestWarp".equals(subChannel)) {
-
             String targetServerName = in.readUTF();
             String warpPointName = in.readUTF();
 
             if (event.getTarget() instanceof Player player) {
 
-                Optional<RegisteredServer> targetServer = proxy.getServer(targetServerName);
 
-                if (targetServer.isEmpty()) {
 
-                    // サーバーが存在しないかオフラインの場合
-                    WarpResult result = WarpResult.targetOffline(targetServerName, warpPointName);
-                    sendWarpResultToPaper(player, result);
+                Optional<RegisteredServer> targetServers = proxy.getServer(targetServerName);
+
+                if (targetServers.isEmpty()) {
+                    player.sendMessage(getKickReasonMessage(
+                            "指定されたワープポイントのサーバーが発見できませんでした"
+                    ));
                     return;
                 }
 
-                // キック時に参照できるよう一時保存
                 pendingWarpPoints.put(player.getUniqueId(), warpPointName);
 
-                // ターゲットサーバーへの転送
-                player.createConnectionRequest(targetServer.get()).connect().thenAccept(result -> {
-                    if (result.isSuccessful()) {
-                        sendExecuteTeleportToTarget(player, warpPointName);
+                RegisteredServer targetServer = targetServers.get();
+
+
+
+                player.createConnectionRequest(targetServer).connect().thenAccept(result -> {
+                    if (!result.isSuccessful()) {
                         pendingWarpPoints.remove(player.getUniqueId());
-                    } else {
-                        pendingWarpPoints.remove(player.getUniqueId());
-                        WarpResult warpResult = WarpResult.targetOffline(targetServerName, warpPointName);
-                        sendWarpResultToPaper(player, warpResult);
+                        player.sendMessage(Component.text("サーバーへの移動に失敗しました:PluginMessage").color(NamedTextColor.RED));
                     }
                 });
-
-
             }
+        }
+    }
+
+    @Subscribe
+    public void onServerConnected(ServerConnectedEvent event) {
+        Player player = event.getPlayer();
+
+        if(pendingWarpPoints.containsKey(player.getUniqueId())) {
+
+
+            String warpPointName = pendingWarpPoints.remove(player.getUniqueId());
+
+
+            proxy.getScheduler().buildTask(plugin, () -> {
+
+                if (warpPointName != null) {
+
+                    ByteArrayDataOutput out = ByteStreams.newDataOutput();
+                    out.writeUTF("ExecuteTeleport");
+                    out.writeUTF(warpPointName);
+
+                    event.getServer().sendPluginMessage(MiniCashVelocityLibrary.WARP_CHANNEL, out.toByteArray());
+
+                }
+
+
+
+            }).delay(500, java.util.concurrent.TimeUnit.MILLISECONDS).schedule();
 
 
         }
+
 
     }
 
-
-    /*
-        サーバー転送でキックされた場合
-     */
     @Subscribe
     public void kicked(KickedFromServerEvent event) {
-
         Player player = event.getPlayer();
 
-        // 一時保存していたワープポイント名を取得する
-        String warpPointName = pendingWarpPoints.remove(player.getUniqueId());
-
-        if (warpPointName == null){
-            warpPointName = "";
-        }
-
-        // 初期接続ではなく、ワープ中のサーバー移動によるキックかどうかの判定
-        if (event.getServer().equals(player.getCurrentServer().orElse(null))) {
+        if(!event.kickedDuringServerConnect()){
             return;
         }
 
-        // キック理由の取得
-        String rawKickReason = event.getServerKickReason()
-                .map(reason -> GsonComponentSerializer.gson().serialize(reason))
-                .orElse("Kicked with no reason provided.");
 
-        // プロキシに表示されるエラー表示をされないようにして、元のサーバーにとどまらせる
-        event.setResult(KickedFromServerEvent.Notify.create(
-                net.kyori.adventure.text.Component.empty()
-        ));
+        String warpPointName = pendingWarpPoints.remove(player.getUniqueId());
 
-        // 元いたサーバーにWarpResultを送信
-        WarpResult warpResult = WarpResult.kicked(rawKickReason, warpPointName);
-        sendWarpResultToPaper(player, warpResult);
+        String kickReason = event.getServerKickReason()
+                .map(component -> PlainTextComponentSerializer.plainText().serialize(component))
+                .orElse("不明なエラー");
+
+        Component finalKickReason;
+
+        if (kickReason == null || kickReason.isEmpty()) {
+
+            finalKickReason = getKickReasonMessage("不明なエラーが発生しました");
+
+        }else {
+            finalKickReason = getKickReasonMessage(kickReason);
+        }
+
+        event.setResult(KickedFromServerEvent.Notify.create(finalKickReason));
+
+
 
     }
 
 
-    /**
-     * 転送成功後、移動先のPaperサーバーへテレポート指示を送る
-     */
-    private void sendExecuteTeleportToTarget(Player player, String warpPointName) {
-        player.getCurrentServer().ifPresent(serverConnection -> {
-            ByteArrayDataOutput out = ByteStreams.newDataOutput();
-            out.writeUTF("ExecuteTeleport");
-            out.writeUTF(warpPointName);
 
-            serverConnection.sendPluginMessage(MiniCashVelocityLibrary.WARP_CHANNEL, out.toByteArray());
-        });
+    private Component getKickReasonMessage(String message){
+
+        return Component.text("[").color(NamedTextColor.GRAY)
+                .append(Component.text("MapleWarp").color(NamedTextColor.GOLD)
+                        .append(Component.text("]",NamedTextColor.GRAY)
+                                .append(Component.text(message).color(NamedTextColor.RED)))
+                );
     }
-
-    /**
-     * Paper側へ結果を送り返す
-     */
-    private void sendWarpResultToPaper(Player player, WarpResult result) {
-
-        player.getCurrentServer().ifPresent(serverConnection -> {
-            ByteArrayDataOutput out = ByteStreams.newDataOutput();
-            out.writeUTF("WarpResponse");
-            out.writeUTF(gson.toJson(result));
-
-            serverConnection.sendPluginMessage(MiniCashVelocityLibrary.WARP_CHANNEL, out.toByteArray());
-
-        });
-    }
-
-
 }
+
