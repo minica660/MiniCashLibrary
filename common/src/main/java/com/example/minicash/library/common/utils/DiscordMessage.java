b7ff3ec23f87e6e0ae6fbfef697d239d1c9b05f6
MiniCash <tminecraft2143@gmail.com>
2026-08-18 21:30:45 +0900

    Add files via upload

--- common/src/main/java/com/example/minicash/library/common/utils/DiscordMessage.java
+++ common/src/main/java/com/example/minicash/library/common/utils/DiscordMessage.java
@@ -3,15 +3,16 @@ package com.example.minicash.library.common.utils;
 import com.example.minicash.library.common.response.NormalResponse;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class DiscordMessage {
 
     private JDA jda;
 
-    private String token;
-    private String url;
-    private String channel;
+    private final String token;
+    private final String url;
+    private final String channel;
 
     public DiscordMessage(String token, String url, String channel) {
         this.token = token;
@@ -43,13 +44,25 @@ public class DiscordMessage {
             jda.shutdown();
             return new NormalResponse(true, "Discord BOTを無効化しました");
         } catch (Exception e) {
-            return new NormalResponse(false , e.getMessage());
+            return new NormalResponse(false, e.getMessage());
         }
 
     }
 
     public boolean sendMessage(String message) {
-        return false;
+
+        try {
+
+            TextChannel textChannel = jda.getTextChannelById(channel);
+            if (textChannel != null) {
+                textChannel.sendMessage(message).queue();
+            }
+
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
     }
 
 
