ae0a6d0b297e8250625a0b0d921e88c45c4abfd3
MiniCash <tminecraft2143@gmail.com>
2026-08-21 22:39:28 +0900

    velocity用ボスバーマネージャー機能追加

---
+++ velocity/src/main/java/com/example/minicash/library/velocity/utils/VelocityBossbarUtil.java
@@ -0,0 +1,64 @@
+package com.example.minicash.library.velocity.utils;
+
+import com.example.minicash.library.common.utils.bossbar.BossBarColor;
+import com.example.minicash.library.common.utils.bossbar.BossBarStyle;
+import com.example.minicash.library.common.utils.bossbar.CustomBossbar;
+import com.velocitypowered.api.proxy.Player;
+import net.kyori.adventure.bossbar.BossBar;
+import net.kyori.adventure.text.Component;
+
+public class VelocityBossbarUtil implements CustomBossbar {
+
+    private final Player player;
+    private final BossBar bossBar;
+
+    public VelocityBossbarUtil(Player player, String title, BossBarColor color, BossBarStyle style) {
+        this.player = player;
+        this.bossBar = BossBar.bossBar(
+                Component.text(title),
+                1.0f,
+                convertColor(color),
+                convertStyle(style)
+        );
+    }
+
+    @Override
+    public void setTitle(String title) {
+        bossBar.name(Component.text(title));
+    }
+
+    @Override
+    public void setProgress(float progress) {
+        bossBar.progress(Math.clamp(progress, 0.0f, 1.0f));
+    }
+
+    @Override
+    public void setColor(BossBarColor color) {
+        bossBar.color(convertColor(color));
+    }
+
+    @Override
+    public void setStyle(BossBarStyle style) {
+        bossBar.overlay(convertStyle(style));
+    }
+
+    @Override
+    public void show() {
+        player.showBossBar(bossBar);
+    }
+
+    @Override
+    public void hide() {
+        player.hideBossBar(bossBar);
+    }
+
+
+    private BossBar.Color convertColor(BossBarColor color) {
+        return BossBar.Color.valueOf(color.name());
+    }
+
+    private BossBar.Overlay convertStyle(BossBarStyle style) {
+        return BossBar.Overlay.valueOf(style.name());
+    }
+
+}
